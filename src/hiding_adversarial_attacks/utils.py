b9bcf7e398fda112f2425b4552328a503058c549
Stefanie Stoppel <s.stoppel@gmx.de>
2021-03-29 16:58:14 +0200

    feat: add util functions

--- src/hiding_adversarial_attacks/utils.py
+++ src/hiding_adversarial_attacks/utils.py
@@ -1,5 +1,12 @@
+import argparse
 from functools import wraps
 from time import time
+
+import matplotlib.pyplot as plt
+import torch
+from torchvision.transforms import ToPILImage
+
+toPilImage = ToPILImage()
 
 
 def timeit(func):
@@ -12,3 +19,23 @@ def timeit(func):
         return result
 
     return wrap
+
+
+class SplitArgs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values.split(","))
+
+
+def display_tensor_as_image(tensor: torch.Tensor):
+    plt.imshow(tensor.numpy()[0], cmap="gray")
+    plt.show()
+
+
+def inverse_normalize(tensor, mean, std):
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
+
+
+def to_pil_image(tensor, mode="L"):
+    return ToPILImage(mode=mode)(tensor)
