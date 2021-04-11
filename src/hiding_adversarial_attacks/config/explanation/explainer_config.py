f02d20d623b3571722e559604f1796e4d61185c7
Stefanie Stoppel <s.stoppel@gmx.de>
2021-04-11 15:20:28 +0200

    feat: add explainer configs

--- src/hiding_adversarial_attacks/config/explanation/explainer_config.py
+++ src/hiding_adversarial_attacks/config/explanation/explainer_config.py
@@ -1,4 +1,24 @@
-class DeepLiftBaselineNames:
-    ZERO = "zero"
-    BLUR = "blur"
-    LOCAL_MEAN = "local_mean"
+from dataclasses import dataclass
+
+from omegaconf import MISSING
+
+from hiding_adversarial_attacks.config.explanation.deep_lift_baseline_config import (
+    DeepLiftBaselineConfig,
+)
+
+
+class ExplainerNames:
+    DEEP_LIFT = "DeepLIFT"
+    GRAD_CAM = "GradCAM"
+
+
+@dataclass
+class ExplainerConfig:
+    name: str = MISSING
+    baseline: DeepLiftBaselineConfig = MISSING
+
+
+@dataclass
+class DeepLiftConfig(ExplainerConfig):
+    name: str = ExplainerNames.DEEP_LIFT
+    baseline: DeepLiftBaselineConfig = MISSING
