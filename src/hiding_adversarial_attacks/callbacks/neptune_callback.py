facb177350f6be0054cf7fc9157c7d3f90ce3dd2
Stefanie Stoppel <s.stoppel@gmx.de>
2021-04-22 13:10:08 +0200

    chore: remove uploading hydra files

--- src/hiding_adversarial_attacks/callbacks/neptune_callback.py
+++ src/hiding_adversarial_attacks/callbacks/neptune_callback.py
@@ -16,7 +16,6 @@ class NeptuneLoggingCallback(Callback):
             self._move_logs_and_outputs(
                 self.log_path, cwd, trainer.logger.name, trainer.logger.version
             )
-            self._upload_hydra_files(trainer)
             self._upload_checkpoints(trainer)
 
     @staticmethod
@@ -27,21 +26,12 @@ class NeptuneLoggingCallback(Callback):
             os.path.join(hydra_experiment_path, "checkpoints"),
             os.path.join(log_path, "checkpoints"),
         )
-        # move hydra yaml files
-        shutil.move(
-            os.path.join(cwd, ".hydra"),
-            os.path.join(log_path, ".hydra"),
-        )
         # move log files
         for log in glob.glob(os.path.join(cwd, "*.log")):
             shutil.move(
                 log,
                 os.path.join(log_path, log),
             )
-
-    def _upload_hydra_files(self, trainer):
-        hydra_dir = os.path.join(self.log_path, ".hydra")
-        trainer.logger.experiment.log_artifact(hydra_dir, "hydra")
 
     def _upload_checkpoints(self, trainer):
         for checkpoint in glob.glob(
