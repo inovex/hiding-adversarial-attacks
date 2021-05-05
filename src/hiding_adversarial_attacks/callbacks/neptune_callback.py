f66d1c41e25aa6f346f2f68d5a45fe3fe579d1b2
Stefanie Stoppel <s.stoppel@gmx.de>
2021-05-05 16:32:18 +0200

    feat: copy checkpoints and log files instead of moving

--- src/hiding_adversarial_attacks/callbacks/neptune_callback.py
+++ src/hiding_adversarial_attacks/callbacks/neptune_callback.py
@@ -14,23 +14,28 @@ class NeptuneLoggingCallback(Callback):
     def on_train_end(self, trainer, pl_module):
         cwd = os.getcwd()
         if not self.trash_run:
-            self._move_logs_and_outputs(
+            self._copy_logs_and_outputs(
                 self.log_path, cwd, trainer.logger.name, trainer.logger.version
             )
-            self._upload_image_log(trainer)
-            self._upload_checkpoints(trainer)
+            # self._upload_image_log(trainer)
+            # self._upload_checkpoints(trainer)
 
     @staticmethod
-    def _move_logs_and_outputs(log_path, cwd, experiment_name, run_id):
+    def _copy_logs_and_outputs(log_path, cwd, experiment_name, run_id):
         hydra_experiment_path = os.path.join(cwd, experiment_name, run_id)
-        # move model checkpoints
-        shutil.move(
-            os.path.join(hydra_experiment_path, "checkpoints"),
-            os.path.join(log_path, "checkpoints"),
-        )
-        # move log files
+        # copy model checkpoints
+        checkpoint_src = os.path.join(hydra_experiment_path, "checkpoints")
+        checkpoints_dest = os.path.join(log_path, "checkpoints")
+        os.makedirs(checkpoints_dest, exist_ok=True)
+        for checkpoint in glob.glob(os.path.join(checkpoint_src, "*.ckpt")):
+            shutil.copy(
+                checkpoint,
+                os.path.join(checkpoints_dest, os.path.basename(checkpoint)),
+            )
+
+        # copy log files
         for log in glob.glob(os.path.join(cwd, "*.log")):
-            shutil.move(
+            shutil.copy(
                 log,
                 os.path.join(log_path, os.path.basename(log)),
             )
