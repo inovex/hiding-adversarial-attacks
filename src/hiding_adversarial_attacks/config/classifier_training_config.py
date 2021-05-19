8bf8e2b01f5d9f143fdd311841d6f84f28e2df7d
Stefanie Stoppel <s.stoppel@gmx.de>
2021-05-19 10:06:58 +0200

    feat: update config to include optuna options, lr and gamma

--- src/hiding_adversarial_attacks/config/classifier_training_config.py
+++ src/hiding_adversarial_attacks/config/classifier_training_config.py
@@ -1,12 +1,13 @@
 import os
 from dataclasses import dataclass, field
-from typing import Any, List
+from typing import Any, List, Optional
 
 from hydra.core.config_store import ConfigStore
 from omegaconf import MISSING
 
 from hiding_adversarial_attacks.config.classifiers.classifier_config import (
     Cifar10ClassifierConfig,
+    ClassifierCheckpointConfig,
     ClassifierConfig,
     FashionMNISTClassifierConfig,
     MNISTClassifierConfig,
@@ -23,14 +24,36 @@ defaults = [{"data_set": "MNIST"}, {"classifier": "MNISTClassifier"}]
 
 
 @dataclass
+class OptunaConfig:
+    # General options
+    use_optuna: bool = True
+    prune_trials: bool = True
+    number_of_trials: int = 10
+    timeout: Optional[int] = None
+
+    # Search spaces for hyperparameters
+    search_space: Any = field(
+        default_factory=lambda: {
+            "lr": {
+                "log": True,
+                "low": 1e-7,
+                "high": 1e-4,
+            },
+            "batch_size": [16, 32, 64],
+        },
+    )
+
+
+@dataclass
 class ClassifierTrainingConfig:
     name: str = "ClassifierTrainingConfig"
 
     random_seed: int = 42
     gpus: int = 1
     test: bool = False
-    checkpoint: str = ""
 
+    lr: float = 0.01
+    gamma: float = 0.07
     batch_size: int = 64
     val_split: float = 0.1
     download: bool = False
@@ -38,17 +61,27 @@ class ClassifierTrainingConfig:
     data_set: DataSetConfig = MISSING
     classifier: ClassifierConfig = MISSING
 
+    # Config for checkpoints
+    checkpoint: str = ""
+    checkpoint_config: ClassifierCheckpointConfig = ClassifierCheckpointConfig()
+    resume_from_checkpoint: bool = False
+
     defaults: List[Any] = field(default_factory=lambda: defaults)
 
     # Path where logs will be saved / moved to
     log_path: str = os.path.join(LoggingConfig.log_root, "train_classifier")
 
     # Set this to False if you want your checkpoints to be saved to Neptune
-    trash_run: bool = True
+    trash_run: bool = False
 
     # Neptune options
     # Tag 'trash' will be added to tags if trash_run is True
     tags: List[str] = field(default_factory=lambda: ["train-classifier"])
+
+    max_epochs: int = 10
+
+    # Optuna options
+    optuna: OptunaConfig = OptunaConfig()
 
 
 cs = ConfigStore.instance()
@@ -57,7 +90,9 @@ cs.store(group="data_set", name="FashionMNIST", node=FashionMNISTConfig)
 cs.store(group="data_set", name="Cifar10", node=Cifar10Config)
 cs.store(group="classifier", name="MNISTClassifier", node=MNISTClassifierConfig)
 cs.store(
-    group="classifier", name="FashionMNISTClassifier", node=FashionMNISTClassifierConfig
+    group="classifier",
+    name="FashionMNISTClassifier",
+    node=FashionMNISTClassifierConfig,
 )
 cs.store(group="classifier", name="Cifar10Classifier", node=Cifar10ClassifierConfig)
 cs.store(name="classifier_training_config", node=ClassifierTrainingConfig)
