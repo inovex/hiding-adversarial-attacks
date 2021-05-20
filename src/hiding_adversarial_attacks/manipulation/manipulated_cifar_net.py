b1ba28177867824f203419f0583a569c8d4e52b0
Stefanie Stoppel <s.stoppel@gmx.de>
2021-05-20 15:17:14 +0200

    feat: add manipulated_cifar_net.py

---
+++ src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+from hiding_adversarial_attacks.classifiers.mnist_net import MNISTNet
+from hiding_adversarial_attacks.manipulation.manipulated_mnist_net import (
+    ManipulatedMNISTNet,
+)
+
+
+class ManipulatedCIFARNet(ManipulatedMNISTNet):
+    def __init__(self, model: MNISTNet, hparams):
+        super().__init__(model=model, hparams=hparams)
