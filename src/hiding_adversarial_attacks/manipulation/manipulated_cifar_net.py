86f07add3bd199a0d09aae9e43a6ec6092a0e72c
Stefanie Stoppel <s.stoppel@gmx.de>
2021-05-23 11:10:43 +0200

    feat: add optimizer in manipulated_cifar_net.py

--- src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
+++ src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import torch
+
 from hiding_adversarial_attacks.classifiers.mnist_net import MNISTNet
 from hiding_adversarial_attacks.manipulation.manipulated_mnist_net import (
     ManipulatedMNISTNet,
@@ -9,3 +11,8 @@ from hiding_adversarial_attacks.manipulation.manipulated_mnist_net import (
 class ManipulatedCIFARNet(ManipulatedMNISTNet):
     def __init__(self, model: MNISTNet, hparams):
         super().__init__(model=model, hparams=hparams)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        # scheduler = StepLR(optimizer, step_size=1, gamma=self.gamma)
+        return [optimizer]
