56d83f4e5db40f9e03e40d45fe06bd58c33fca8f
Stefanie Stoppel <s.stoppel@gmx.de>
2021-06-21 11:22:09 +0200

    feat: use module function in network instead of torch functional

--- src/hiding_adversarial_attacks/classifiers/mnist_net.py
+++ src/hiding_adversarial_attacks/classifiers/mnist_net.py
@@ -37,6 +37,10 @@ class MNISTNet(pl.LightningModule):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.relu3 = nn.ReLU()
+        self.max_pool2d = nn.MaxPool2d(2)
 
     def _setup_metrics(self):
         # Classification accuracy
@@ -62,14 +66,14 @@ class MNISTNet(pl.LightningModule):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
+        x = self.relu1(x)
         x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        x = self.relu2(x)
+        x = self.max_pool2d(x)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = F.relu(x)
+        x = self.relu3(x)
         x = self.dropout2(x)
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
