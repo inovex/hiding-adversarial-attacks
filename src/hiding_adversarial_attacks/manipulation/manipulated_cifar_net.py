528d8ec6cd37671c61dc23d0678efd88214ccc5a
Stefanie Stoppel <s.stoppel@gmx.de>
2021-07-05 13:42:29 +0200

    feat: remove type

--- src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
+++ src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
@@ -2,17 +2,15 @@ from __future__ import print_function
 
 import torch
 
-from hiding_adversarial_attacks.classifiers.mnist_net import MNISTNet
 from hiding_adversarial_attacks.manipulation.manipulated_mnist_net import (
     ManipulatedMNISTNet,
 )
 
 
 class ManipulatedCIFARNet(ManipulatedMNISTNet):
-    def __init__(self, model: MNISTNet, hparams):
+    def __init__(self, model, hparams):
         super().__init__(model=model, hparams=hparams)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        # scheduler = StepLR(optimizer, step_size=1, gamma=self.gamma)
         return [optimizer]
