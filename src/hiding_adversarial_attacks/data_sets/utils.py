78396e4b5a5cee2edf948e91b58c33e058b92b69
Stefanie Stoppel <s.stoppel@gmx.de>
2021-07-09 17:34:07 +0200

    fix: transform creation

--- src/hiding_adversarial_attacks/data_sets/utils.py
+++ src/hiding_adversarial_attacks/data_sets/utils.py
@@ -6,7 +6,7 @@ from hiding_adversarial_attacks.config.data_sets.data_set_config import DataSetN
 def get_transform(data_set_name: str, data_is_tensor: bool = False):
     transform = []
     if not data_is_tensor:
-        transform = transform.append(transforms.ToTensor())
+        transform.append(transforms.ToTensor())
     if DataSetNames.CIFAR10 in data_set_name:
         mean = [0.4914, 0.4822, 0.4465]
         std = [0.2023, 0.1994, 0.2010]
