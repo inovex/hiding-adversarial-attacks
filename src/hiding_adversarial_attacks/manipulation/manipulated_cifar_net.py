9e78d31499100a1fd255b9591400f8046479d26c
Stefanie Stoppel <s.stoppel@gmx.de>
2021-08-07 19:10:57 +0200

    feat: add StepLR and config

--- src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
+++ src/hiding_adversarial_attacks/manipulation/manipulated_cifar_net.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import torch
+from torch.optim.lr_scheduler import StepLR
 
 from hiding_adversarial_attacks.manipulation.manipulated_mnist_net import (
     ManipulatedMNISTNet,
@@ -12,5 +13,16 @@ class ManipulatedCIFARNet(ManipulatedMNISTNet):
         super().__init__(model=model, hparams=hparams)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.lr,
+            weight_decay=self.hparams["weight_decay"],
+        )
+        if self.hparams["steps_lr"] > 0 and self.hparams["gamma"] != 1.0:
+            scheduler = StepLR(
+                optimizer,
+                step_size=self.hparams["steps_lr"],
+                gamma=self.hparams["gamma"],
+            )
+            return [optimizer], [scheduler]
         return [optimizer]
