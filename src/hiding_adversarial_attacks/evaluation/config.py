aa7f247f10afce432866220b2e4837790726ec63
Stefanie Stoppel <s.stoppel@gmx.de>
2021-10-05 17:02:44 +0200

    feat: change y limits of similarity plots of SSIM to None

--- src/hiding_adversarial_attacks/evaluation/config.py
+++ src/hiding_adversarial_attacks/evaluation/config.py
@@ -7,4 +7,4 @@ SIMILARITIES_FILE = "test_similarities.csv"
 SIMILARITIES_COLS = ["pcc_sim", "mse_sim", "ssim_sim"]
 Y_LABELS = ["PCC", "MSE", "SSIM"]
 COLOR_PALETTES = [PCC_COLOR_PALETTE, "Oranges", SSIM_COLOR_PALETTE]
-SIM_Y_LIMITS = [(-1.1, 1.1), None, (-0.1, 1.1)]
+SIM_Y_LIMITS = [(-1.1, 1.1), None, None]
